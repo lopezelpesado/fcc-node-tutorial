29da40f0b0a125b6cbcb34ce91a70349625ed8ed
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2022-01-06 15:19:19 +0000

    trying out globals

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const amount = 12;
-
-if (amount < 10) {
-    console.log("small number");
-} else {
-    console.log("large number");
-}
-
-console.log("Hello world!");
+console.log(__dirname);
+setInterval(() => {
+    console.log("yeet");
+}, 1000)
