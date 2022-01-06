3ddce710f526e3ed63c0b92750b6e4f4a50b53e7
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2022-01-06 16:19:25 +0000

    fs module

---
+++ 10-fs-sync.js
@@ -0,0 +1,11 @@
+const {readFileSync, writeFileSync} = require("fs");
+console.log("start");
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
+
+writeFileSync("./content/result-sync.txt", `Here is the result: ${first}, ${second}`, {flag: "a"})
+
+console.log(first, second);
+
+console.log("done this task");
+console.log("starting the next one");
