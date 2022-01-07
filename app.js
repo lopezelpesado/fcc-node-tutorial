e33cba4fbbf578c0bc667104370b09707131f90d
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2022-01-07 16:01:32 +0000

    gitignore and npm

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const http = require("http");
+const _ = require("lodash");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/" || req.url === "") {
-        res.end("Welcome to our home page");
-    } else if (req.url === "/about") {
-        res.end("Here is our short history");
-    } else {
-        res.end(`
-            <h1>Oops</h1>
-            <p>I'm sorry, I can't do that.</p>
-            <a href="/">Go home loser!</a>
-        `)
-    }
-})
+const items = [1, [2, [3, [4]]]];
 
-server.listen(5000);
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
