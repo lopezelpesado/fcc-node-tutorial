8664338eb1193cdcbf324ed7a7481555bf300052
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2022-01-07 17:01:53 +0000

    async await

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const _ = require("lodash");
+const { readFile, writeFile } = require("fs").promises;
 
-const items = [1, [2, [3, [4]]]];
+// const util = require("util");
 
-const newItems = _.flattenDeep(items);
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-console.log(newItems);
 
-console.log("hello world");
+const start = async () => {
+    try {
+        const first = await readFile("./content/first.txt", "utf-8");
+        const second = await readFile("./content/second.txt", "utf-8");
+        await writeFile("./content/result-mind-grenade.txt", `THIS IS AWESOME: ${first} ${second}`, {flag: "a"})
+        console.log(first, second);  
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+// getText("./content/first.txt")
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, "utf-8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+// }
